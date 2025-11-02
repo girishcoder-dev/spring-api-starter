f73fef7e73cab8d266881753b164430be0af1d45
Girish Suri <girishgeneral@gmail.com>
2025-11-02 11:48:30 +0530

    Built the product endpoints

---
+++ src/main/java/com/codewithmosh/store/dtos/ProductDto.java
@@ -0,0 +1,18 @@
+package com.codewithmosh.store.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@Data
+public class ProductDto {
+
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private String description;
+    private Byte categoryId;
+
+}
