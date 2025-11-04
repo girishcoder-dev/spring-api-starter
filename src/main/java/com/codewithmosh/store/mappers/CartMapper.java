d7fb472737ad24ead12cabe12bc4383b37aed0c8
Girish Suri <girishgeneral@gmail.com>
2025-11-04 22:50:25 +0530

    Add logic to get total price for a cart item

--- src/main/java/com/codewithmosh/store/mappers/CartMapper.java
+++ src/main/java/com/codewithmosh/store/mappers/CartMapper.java
@@ -6,10 +6,13 @@ import com.codewithmosh.store.dtos.CartItemDto;
 import com.codewithmosh.store.entities.Cart;
 import com.codewithmosh.store.entities.CartItem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
 
     CartDto toDto(Cart cart);
+
+    @Mapping(target = "totalPrice", expression = "java(cartItem.getTotalPrice())")
     CartItemDto toDto(CartItem cartItem);
 }
