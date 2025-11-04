e7cf6afd975977cdfaeafc614bd17455ec77a231
Girish Suri <girishgeneral@gmail.com>
2025-11-04 09:49:10 +0530

    Added code to validate business rules

--- src/main/java/com/codewithmosh/store/repositories/UserRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/UserRepository.java
@@ -1,7 +1,10 @@
 package com.codewithmosh.store.repositories;
 
 import com.codewithmosh.store.entities.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
 }
