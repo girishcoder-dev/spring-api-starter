3b13280276dd970efce22959e442b3fb66a776a1
Girish Suri <girishgeneral@gmail.com>
2025-11-04 19:09:10 +0530

    Creating a cart logic

---
+++ src/main/java/com/codewithmosh/store/mappers/CartMapper.java
@@ -0,0 +1,12 @@
+package com.codewithmosh.store.mappers;
+
+
+import com.codewithmosh.store.dtos.CartDto;
+import com.codewithmosh.store.entities.Cart;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CartMapper {
+
+    CartDto toDto(Cart cart);
+}
