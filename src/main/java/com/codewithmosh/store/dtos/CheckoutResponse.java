76b2c6e19e201b754b36bb82e559a579dcb39fb2
Girish Suri <girishgeneral@gmail.com>
2025-11-09 12:54:48 +0530

    Added checkout feature

---
+++ src/main/java/com/codewithmosh/store/dtos/CheckoutResponse.java
@@ -0,0 +1,13 @@
+package com.codewithmosh.store.dtos;
+
+import lombok.Data;
+
+@Data
+public class CheckoutResponse {
+
+    private Long orderId;
+
+    public CheckoutResponse(Long id) {
+        this.orderId = id;
+    }
+}
