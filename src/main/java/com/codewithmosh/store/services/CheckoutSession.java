6b86c9484c860e544ca4ca33d235a4d43d9555b7
Girish Suri <girishgeneral@gmail.com>
2025-11-11 16:17:38 +0530

    Decoupling from Stripe

---
+++ src/main/java/com/codewithmosh/store/services/CheckoutSession.java
@@ -0,0 +1,11 @@
+package com.codewithmosh.store.services;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CheckoutSession {
+
+    private String checkoutUrl;
+}
